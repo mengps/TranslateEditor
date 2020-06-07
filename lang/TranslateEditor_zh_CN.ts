f01c03cbc08133599726be98ee95e845ee962a44
mengps <843261040@qq.com>
2020-06-07 17:05:44 +0800

    update: Complete translation.

--- lang/TranslateEditor_zh_CN.ts
+++ lang/TranslateEditor_zh_CN.ts
@@ -2,131 +2,201 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="zh_CN">
 <context>
-    <name>CodeWindow</name>
+    <name>QObject</name>
     <message>
-        <location filename="../qml/CodeWindow.qml" line="83"/>
-        <source>Close Window</source>
-        <translation type="unfinished"></translation>
+        <location filename="../src/main.cpp" line="22"/>
+        <source>Can&apos;t create TsModel Object!</source>
+        <translatorcomment>不能创建 TsModel 对象!</translatorcomment>
+        <translation>不能创建 TsModel 对象!</translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="23"/>
+        <source>Can&apos;t create Translator Object!</source>
+        <translatorcomment>不能创建 Translator 对象!</translatorcomment>
+        <translation>不能创建 Translator 对象!</translation>
     </message>
 </context>
 <context>
-    <name>QObject</name>
+    <name>Translator</name>
     <message>
-        <location filename="../src/main.cpp" line="20"/>
-        <source>Don&apos;t create TsModel Object!</source>
-        <translation type="unfinished"></translation>
+        <location filename="../src/translator.cpp" line="31"/>
+        <source>--- Language changed to Chinese</source>
+        <translatorcomment>--- 语言改为中文</translatorcomment>
+        <translation>--- 语言改为中文</translation>
+    </message>
+    <message>
+        <location filename="../src/translator.cpp" line="36"/>
+        <source>--- Language changed to English</source>
+        <translatorcomment>--- 语言改为英文</translatorcomment>
+        <translation>--- 语言改为英文</translation>
     </message>
 </context>
 <context>
     <name>TsParser</name>
     <message>
-        <location filename="../src/tsparser.cpp" line="231"/>
+        <location filename="../src/tsparser.cpp" line="239"/>
         <source>This &apos;ts&apos; file not have &lt;TS&gt; element!</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>此&apos;ts&apos;文件没有&lt;TS&gt;元素!</translatorcomment>
+        <translation>此&apos;ts&apos;文件没有&lt;TS&gt;元素!</translation>
     </message>
     <message>
-        <location filename="../src/tsparser.cpp" line="235"/>
+        <location filename="../src/tsparser.cpp" line="243"/>
         <source>Parser start error!</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>解析器启动错误！</translatorcomment>
+        <translation>解析器启动错误！</translation>
     </message>
 </context>
 <context>
     <name>main</name>
     <message>
-        <location filename="../qml/main.qml" line="14"/>
+        <location filename="../qml/main.qml" line="17"/>
         <source>Translate Editor</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>翻译编辑器</translatorcomment>
+        <translation>翻译编辑器</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="56"/>
+        <location filename="../qml/main.qml" line="81"/>
         <source>File</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>文件</translatorcomment>
+        <translation>文件</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="69"/>
+        <location filename="../qml/main.qml" line="88"/>
         <source>Open [.ts]</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>打开 [.ts]</translatorcomment>
+        <translation>打开 [.ts]</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="77"/>
+        <location filename="../qml/main.qml" line="96"/>
         <source>Please choose a TS file</source>
-        <translation type="unfinished"></translation>
+        <translation>请选择一个TS文件</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="79"/>
-        <location filename="../qml/main.qml" line="107"/>
+        <location filename="../qml/main.qml" line="98"/>
+        <location filename="../qml/main.qml" line="128"/>
         <source>TS files (*.ts *.TS)</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>TS 文件(*.ts *.TS)</translatorcomment>
+        <translation>TS 文件(*.ts *.TS)</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="94"/>
+        <location filename="../qml/main.qml" line="114"/>
         <source>Save [.ts]</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/main.qml" line="102"/>
-        <source>Please Save to TS file</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>保存 [.ts]</translatorcomment>
+        <translation>保存 [.ts]</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="122"/>
+        <source>Please Save to TS file</source>
+        <translatorcomment>请保存到TS文件</translatorcomment>
+        <translation>请保存到TS文件</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="143"/>
         <source>Tool</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>工具</translatorcomment>
+        <translation>工具</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="134"/>
+        <location filename="../qml/main.qml" line="150"/>
         <source>Create [.qm]</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>创建 [.qm]</translatorcomment>
+        <translation>创建 [.qm]</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="147"/>
+        <location filename="../qml/main.qml" line="161"/>
+        <source>Please Create to qm file [lrelease.exe]</source>
+        <translatorcomment>请创建到qm文件[lrelease.exe]</translatorcomment>
+        <translation>请创建到qm文件[lrelease.exe]</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="167"/>
+        <source>QM files (*.qm *.QM)</source>
+        <translatorcomment>QM 文件(*.qm *.QM)</translatorcomment>
+        <translation>QM 文件(*.qm *.QM)</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="177"/>
+        <source>Create as [.qm] file (release translation)</source>
+        <translatorcomment>创建为[.qm]文件（发布翻译）</translatorcomment>
+        <translation>创建为[.qm]文件（发布翻译）</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="188"/>
         <source>Setting</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>设置</translatorcomment>
+        <translation>设置</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="159"/>
+        <location filename="../qml/main.qml" line="195"/>
         <source>Qt Location</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>Qt 位置</translatorcomment>
+        <translation>Qt 位置</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="190"/>
-        <source>Minimize Window</source>
-        <translation type="unfinished"></translation>
+        <location filename="../qml/main.qml" line="203"/>
+        <source>Set the location of [Qt/linguest.exe]</source>
+        <translatorcomment>设置[Qt / linguest.exe]的位置</translatorcomment>
+        <translation>设置[Qt / linguest.exe]的位置</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="209"/>
-        <source>Close Window</source>
-        <translation type="unfinished"></translation>
+        <location filename="../qml/main.qml" line="208"/>
+        <source>Please set the folder where qt is located</source>
+        <translatorcomment>请设置qt所在的文件夹</translatorcomment>
+        <translation>请设置qt所在的文件夹</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="275"/>
+        <location filename="../qml/main.qml" line="223"/>
+        <source>Language</source>
+        <translatorcomment>语言</translatorcomment>
+        <translation>语言</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="232"/>
+        <source>English</source>
+        <translatorcomment>英文</translatorcomment>
+        <translation>英文</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="247"/>
+        <source>Chinese</source>
+        <translatorcomment>中文</translatorcomment>
+        <translation>中文</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="357"/>
         <source>Show Details</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>显示细节</translatorcomment>
+        <translation>显示细节</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="287"/>
+        <location filename="../qml/main.qml" line="370"/>
         <source>Location</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>位置</translatorcomment>
+        <translation>位置</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="299"/>
+        <location filename="../qml/main.qml" line="383"/>
         <source>Source Text</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>源文本</translatorcomment>
+        <translation>源文本</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="311"/>
+        <location filename="../qml/main.qml" line="396"/>
         <source>Translate Text</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>翻译文本</translatorcomment>
+        <translation>翻译文本</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="323"/>
+        <location filename="../qml/main.qml" line="409"/>
         <source>Comment Text</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>注释文本</translatorcomment>
+        <translation>注释文本</translation>
     </message>
     <message>
-        <location filename="../qml/main.qml" line="433"/>
+        <location filename="../qml/main.qml" line="518"/>
         <source>Error!</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>错误!</translatorcomment>
+        <translation>错误!</translation>
     </message>
 </context>
 </TS>

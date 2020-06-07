d8a09911fa27560ef76f8c1a380d3e3599661410
mengps <843261040@qq.com>
2020-06-07 18:11:51 +0800

    update: register Translator.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include "fileapi.h"
 #include "framelesswindow.h"
 #include "tslistmodel.h"
+#include "tsapi.h"
+#include "translator.h"
 
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
@@ -17,11 +19,14 @@ int main(int argc, char *argv[])
 
     qmlRegisterType<FramelessWindow>("an.window", 1, 0, "FramelessWindow");
     qmlRegisterType<DocumentHelper>("an.document", 1, 0, "DocumentHelper");
-    qmlRegisterUncreatableType<TsListModel>("an.model", 1, 0, "TsModel", QObject::tr("Don't create TsModel Object!"));
+    qmlRegisterUncreatableType<TsListModel>("an.model", 1, 0, "TsModel", QObject::tr("Can't create TsModel Object!"));
+    qmlRegisterUncreatableType<Translator>("an.translate", 1, 0, "Language", QObject::tr("Can't create Translator Object!"));
 
     QQmlApplicationEngine engine;
     engine.rootContext()->setContextProperty("tsModel", new TsListModel);
     engine.rootContext()->setContextProperty("fileApi", FileApi::instance());
+    engine.rootContext()->setContextProperty("tsApi", TsApi::instance());
+    engine.rootContext()->setContextProperty("translator", Translator::instance(&engine));
     const QUrl url(QStringLiteral("qrc:/qml/main.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
